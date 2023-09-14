4954f3d91b91cf63058aa8bb2233100173ab08bf
jeffresanovich <jeffresanovich@gmail.com>
2023-09-14 08:12:19 -0300

    created productDatail screen

---
+++ src/screens/ProductDetail.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+import products from "../data/products";
+
+const ProductDetail = ({ product }) => {
+  return (
+    <View>
+      <Text>{product.}</Text>
+    </View>
+  );
+};
+
+export default ProductDetail;
+
+const styles = StyleSheet.create({});
