4e06048135be79bc33531d8f7720a268ef38435a
Jeffrey <jeffresanovich@gmail.com>
2023-09-16 15:31:19 -0300

    product not found

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,19 +1,25 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, Text } from "react-native";
 
 import ProductItem from "./ProductItem";
 
 const ProductsList = ({ products, navigation }) => {
   return (
     <View>
-      <FlatList
-        data={products}
-        keyExtractor={products.id}
-        renderItem={({ item }) => (
-          <ProductItem item={item} navigation={navigation} />
-        )}
-      />
+      {products.lenght < 0 ? (
+        <FlatList
+          data={products}
+          keyExtractor={products.id}
+          renderItem={({ item }) => (
+            <ProductItem item={item} navigation={navigation} />
+          )}
+        />
+      ) : (
+        <Text>Sin productos para mostar</Text>
+      )}
     </View>
   );
 };
 
 export default ProductsList;
+
+/**ESTILIZAR EL COMPONENTE "SIN PRODUCTOS PARA MOSTRAR" */
